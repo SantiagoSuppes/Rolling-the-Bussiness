5ee23c3d13d6722b241f7546b5fd5b2640d9df9c
Santiago Suppes <slsuppes@gmail.com>
2025-06-04 22:31:21 -0300

    routes corrections

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import CartWidget from "./CartWidget";
 const NavBar = () => {
     
     return (
-        <nav className="bg-amber-400/90 flex justify-items-stretch items-center px-2 py-4 text-center max-h-28">
+        <nav className="bg-amber-400/90 flex items-center px-2 py-4 text-center max-h-28">
 
             <div className="flex-none">
                 <img src="src/assets/RtB-logo.png" className="w-20 h-12 xs:w-28 xs:h-14 sm:w-36 sm:h-18 md:w-52 md:h-23 lg:w-66 lg:h-28 items-center ml-2 max-w-120 cursor-pointer"></img>
